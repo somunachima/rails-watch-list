1d37863076ff7ce48da12548d505cdf7f55ba85a
Som Akudu <somakudu@gmail.com>
2022-11-18 11:06:42 +0000

    Started on SCSS styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:new, :create, :show, :index] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
 
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
